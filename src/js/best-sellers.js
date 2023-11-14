18bcb880e5833807fe557623b4abb157af6858fd
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2023-11-14 18:45:36 +0200

    Update best-sellers.js

--- src/js/best-sellers.js
+++ src/js/best-sellers.js
@@ -1,61 +1,6 @@
-// import { API_SERVICE} from "./api-requests";
-// // import { openModalId } from "./modals";
-// //
-// const topBooksByCategories = document.querySelector('.best-sellers-books')
-// const apiCategories = new API_SERVICE();
-
-//  bestSellersMarkup();
-
-// async function bestSellersMarkup () {
-//     let result = await apiCategories
-//     .fetchBestSellersBooks()
-//     // console.log(result)
-//     .then(topCategories => {
-//         return topCategories.data
-//         .map((topCategory) => {
-//             // const {book} = bookAttributes;
-
-//             // console.log(topCategory);
-//             const booksArr = topCategory.books.map((book)=>
-//              `<li class="best-sellers-book js-open-modal" data-book-id="${book._id}">
-//              <img class="best-sellers-book-img" src="${book.book_image}" alt="${book.title}" />
-//              <p class="best-sellers-book-title">${book.title}</p>
-//              <p class="best-sellers-book-author">${book.author}</p>
-//              <p class="book-overlay">Quick view</p>
-// </li>`
-//             ).join('');
-//             // console.log(booksArr);
-
-//             topBooksByCategories.insertAdjacentHTML('beforeend',
-//             `<li class="best-seller-category">
-//                 <p class="category-title"> ${topCategory.list_name} </p>
-//                 <ul class="best-sellers-list-in-category">
-//                 ${booksArr}
-//                 </ul>
-//                 <button type="button" class="best-sellers-see-more-button" data-button-category-name="${topCategory.list_name}">See more</button>
-//                     </li>`)
-//         }).join('')
-//     })
-//     .then(()=>{
-//        const openBookEl = document.querySelectorAll('.js-open-modal');
-//         openBookEl.forEach(function(item){
-//                 //  console.log(item);
-//                  item.addEventListener('click', openModalId);
-//     })
-//     })
-//     .catch(error => {
-//         console.log(error.message);
-//         topBooksByCategories.innerHTML = 'No information';
-//       });
-
-//     return result;
-//     }
-
-//     export{bestSellersMarkup};
-
 import { API_SERVICE } from './api-requests';
-// import Notiflix from "notiflix";
-
+// import { openModalId } from "./modals";
+//
 const topBooksByCategories = document.querySelector('.best-sellers-books');
 const apiCategories = new API_SERVICE();
 
@@ -74,10 +19,11 @@ async function bestSellersMarkup() {
           const booksArr = topCategory.books
             .map(
               book =>
-                `<li class="best-sellers-book" data-book-id="${book._id}">
+                `<li class="best-sellers-book js-open-modal" data-book-id="${book._id}">
              <img class="best-sellers-book-img" src="${book.book_image}" alt="${book.title}" />
              <p class="best-sellers-book-title">${book.title}</p>
              <p class="best-sellers-book-author">${book.author}</p>
+             <p class="book-overlay">Quick view</p>
 </li>`
             )
             .join('');
@@ -96,6 +42,13 @@ async function bestSellersMarkup() {
         })
         .join('');
     })
+    .then(() => {
+      const openBookEl = document.querySelectorAll('.js-open-modal');
+      openBookEl.forEach(function (item) {
+        //  console.log(item);
+        item.addEventListener('click', openModalId);
+      });
+    })
     .catch(error => {
       console.log(error.message);
       topBooksByCategories.innerHTML = 'No information';
@@ -103,3 +56,5 @@ async function bestSellersMarkup() {
 
   return result;
 }
+
+export { bestSellersMarkup };
