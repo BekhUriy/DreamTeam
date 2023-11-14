370527ab26b7a88a763e44b5fc11b4eb69fa8562
poliHorser <134537683+poliHorser@users.noreply.github.com>
2023-11-14 15:10:22 +0200

    go

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './js/about-book-modal.js';
+
 import './js/api-requests.js';
 import './js/best-sellers.js';
 import './js/category-list.js';
@@ -6,7 +6,6 @@ import './js/category-markup.js';
 import './js/dark-theme.js';
 import './js/header.js';
 import './js/modal.js';
-import './js/modals.js';
 import './js/render-book.js';
 import './js/rent-block.js';
 import './js/scrollBtn.js';
