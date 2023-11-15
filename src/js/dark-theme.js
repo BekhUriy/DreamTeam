17e9f7ef1496ae93d141d1adf0528f911260caa6
Volodymyr <vovkul@gmail.com>
2023-11-15 01:22:07 +0100

    add switch theme

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -1,90 +1,29 @@
-// const root = document.querySelector(':root')
-// const themeSwitch = document.querySelector('#themeSwitch');
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToogle = document.querySelector('#themeSwitch')
 
-// themeSwitch.addEventListener('change', changeTheme);
 
-// function changeTheme() {
-//     event.preventDefault();
-//     root.classList.toggle('dark-theme', themeSwitch.checked);
-// }
-
-const lightStyles = document.querySelectorAll('link[rel=stylesheet][media*=prefers-color-scheme][media*=light]');
-const darkStyles = document.querySelectorAll('link[rel=stylesheet][media*=prefers-color-scheme][media*=dark]');
-const darkSchemeMedia = matchMedia('(prefers-color-scheme: dark)');
-const themeSwitch = document.querySelector('#themeSwitch');
-
-function setupSwitcher() {
-    const savedScheme = getSavedScheme();
-
-    if (savedScheme !== null) {
-        themeSwitch.checked = savedScheme === 'dark';
-    }
-
-    themeSwitch.addEventListener('change', (event) => {
-        const newScheme = event.target.checked ? 'dark' : 'light';
-        setScheme(newScheme);
-    });
+function enableDarkMode () {
+    document.body.classList.add('darkmode');
+    localStorage.setItem('darkMode', 'enabled')
 }
 
-function setupScheme() {
-    const savedScheme = getSavedScheme();
-    const systemScheme = getSystemScheme();
-
-    if (savedScheme === null) return;
-
-    if (savedScheme !== systemScheme) {
-        setScheme(savedScheme);
-    }
+function disableDarkMode  () {
+    document.body.classList.remove('darkmode');
+    localStorage.setItem('darkMode', null)
 }
 
-function setScheme(scheme) {
-    switchMedia(scheme);
-
-    if (scheme === 'auto') {
-        clearScheme();
-    } else {
-        saveScheme(scheme);
-    }
+if (darkMode === 'enadled') {
+    enableDarkMode()
 }
 
-function switchMedia(scheme) {
-    let lightMedia;
-    let darkMedia;
+darkModeToogle.addEventListener('change', () => {
 
-    if (scheme === 'auto') {
-        lightMedia = '(prefers-color-scheme: light)';
-        darkMedia = '(prefers-color-scheme: dark)';
-    } else {
-        lightMedia = (scheme === 'light') ? 'all' : 'not all';
-        darkMedia = (scheme === 'dark') ? 'all' : 'not all';
-    }
-
-    lightStyles.forEach((link) => {
-        link.media = lightMedia;
-    });
-
-    darkStyles.forEach((link) => {
-        link.media = darkMedia;
-    });
+    darkMode=localStorage.getItem('darkMode')
+if (darkMode!=='enabled') {
+    enableDarkMode()
+    // console.log(darkMode);
+} else {
+    disableDarkMode()
+    // console.log(darkMode);
 }
-
-function getSystemScheme() {
-    const darkScheme = darkSchemeMedia.matches;
-
-    return darkScheme ? 'dark' : 'light';
-}
-
-function getSavedScheme() {
-    return localStorage.getItem('color-scheme');
-}
-
-function saveScheme(scheme) {
-    localStorage.setItem('color-scheme', scheme);
-}
-
-function clearScheme() {
-    localStorage.removeItem('color-scheme');
-}
-
-setupSwitcher();
-setupScheme();
+})
