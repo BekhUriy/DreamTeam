f277ac9918776f0929572cfa6428d5473defd0df
BekhUriy <131916270+BekhUriy@users.noreply.github.com>
2023-11-17 14:04:40 +0000

    Deploying to gh-pages from @ BekhUriy/DreamTeam@c30abdaddb22efdf3b9a2e2de22e3571dd0cc0ba 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,7 +1,7 @@
-import"./assets/support-slider-726a0b2c.js";const l=document.querySelector(".shopping-list-cards");document.querySelector(".shopping-list-btn-delete");const i=document.querySelector(".shopping-list-storage");document.deleteCardItem=function(e){let s=a(n);s=s.filter(o=>o.id!==e),localStorage.setItem(n,JSON.stringify(s)),document.getElementById(""+e).remove(),s.length===0&&(l.style.display="none",i.style.display="block")};function p(t){return t.map(e=>`
+import"./assets/support-slider-c7be306d.js";const l=document.querySelector(".shopping-list-cards");document.querySelector(".shopping-list-btn-delete");const i=document.querySelector(".shopping-list-storage");document.deleteCardItem=function(e){let s=a(n);s=s.filter(o=>o.id!==e),localStorage.setItem(n,JSON.stringify(s)),document.getElementById(""+e).remove(),s.length===0&&(l.style.display="none",i.style.display="block")};function p(t){return t.map(e=>`
   <li class="shopping-list-card" id="${e.id}">
-        <div class="shopping-list-card-img">
-          <img src="${e.book_image}" alt="${e.title}"/>
+       <div class="shopping-list-card-img">
+          <img src="${e.book_image}" class="shopping-list-img" alt="${e.title}"/>
         </div>
         <div class="shopping-list-blok">
           <h2 class="shopping-list-book-title">"${e.title}"</h2>
@@ -16,7 +16,7 @@ import"./assets/support-slider-726a0b2c.js";const l=document.querySelector(".sho
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src="./img/amazon.png" class="amazon" alt="${e.marketAmazon}" />
+              <img src="../img/amazon.jpg" class="amazon" alt="${e.marketAmazon}" />
             </a>
           </li>
           <li class="shopping-list-shop">
@@ -25,7 +25,7 @@ import"./assets/support-slider-726a0b2c.js";const l=document.querySelector(".sho
               target="_blank"
               rel="noopener noreferrer"
             >
-              <img src="./img/appleBook.png" class="appleBook" alt="${e.marketAppleBooks}" />
+              <img src="../img/appleBook.jpg" class="appleBook" alt="${e.marketAppleBooks}" />
             </a>
           </li>
         </ul>
@@ -34,9 +34,9 @@ import"./assets/support-slider-726a0b2c.js";const l=document.querySelector(".sho
           onClick = "deleteCardItem('${e.id}')"
           class="shopping-list-btn-delete"
         >
-          <svg class="shopping-list-btn-delete-icon" width="18" height="18">
-            <use href=./img/sprit.svg#icon-trash-03"></use>
+          <svg class="shopping-list-btn-delete-icon" width="16" height="16">
+            <use href="../img/sprit.svg#icon-trash"></use>
           </svg>
         </button>
-      </li>`).join("")}const n="storage-data";function a(t){const e=localStorage.getItem(t);return e===null?[]:JSON.parse(e)}function r(){const t=a(n);t&&t.length>0?(i.style.display="none",l.innerHTML=p(t)):(l.style.display="none",i.style.display="block")}r();
+  </li>`).join("")}const n="storage-data";function a(t){const e=localStorage.getItem(t);return e===null?[]:JSON.parse(e)}function r(){const t=a(n);t&&t.length>0?(i.style.display="none",l.innerHTML=p(t)):(l.style.display="none",i.style.display="block")}r();
 //# sourceMappingURL=commonHelpers2.js.map
