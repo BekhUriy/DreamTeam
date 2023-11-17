ab2fd124086549e3855cf6b4a236da6a58af736b
BekhUriy <131916270+BekhUriy@users.noreply.github.com>
2023-11-17 07:27:42 +0000

    Deploying to gh-pages from @ BekhUriy/DreamTeam@6a31375f3e848c502a4ecd607dc96c7570f57261 🚀

--- assets/support-slider-fb34cdb4.js
+++ assets/support-slider-fb34cdb4.js
@@ -48,4 +48,4 @@
                     <div class="num-order">${l}</div>
                 <img src="./img/support-ua/${a}.png" alt="${s}" srcset="./img/support-ua/${a}.png 1x, ./img/support-ua/${a}@2x.png 2x">
             </a></li>`}).join("");document.querySelector(".fonds").insertAdjacentHTML("beforeend",t)}G(D);export{Z as o};
-//# sourceMappingURL=support-slider-ce20dd78.js.map
+//# sourceMappingURL=support-slider-fb34cdb4.js.map
