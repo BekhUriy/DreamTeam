91ad5a189e388240037e5989d7a9d9ead2f99367
Bekhuriy <131916270+Bekhuriy@users.noreply.github.com>
2023-11-17 05:04:27 +0200

    Update shopping-list.js

--- src/shopping-list.js
+++ src/shopping-list.js
@@ -5,4 +5,4 @@ import './js/header.js';
 import './js/rent-block.js';
 // import './js/scrollBtn.js';
 import './js/support-slider.js';
-import './js/modal-about.js';
+// import './js/modal-about.js';
