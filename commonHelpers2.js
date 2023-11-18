c1b8f44137d6a0fe5969ea801e7a2f72716f0d00
BekhUriy <131916270+BekhUriy@users.noreply.github.com>
2023-11-18 13:17:05 +0000

    Deploying to gh-pages from @ BekhUriy/DreamTeam@1e74927d5100cd2e14f3407f05d30634e37b3054 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,38 +1,46 @@
-import"./assets/support-slider-ee0ae5e3.js";const l=document.querySelector(".shopping-list-cards");document.querySelector(".shopping-list-btn-delete");const i=document.querySelector(".shopping-list-storage");document.deleteCardItem=function(t){let s=o(a);s=s.filter(n=>n.id!==t),localStorage.setItem(a,JSON.stringify(s)),document.getElementById(""+t).remove(),s.length===0&&(l.style.display="none",i.style.display="block")};function p(e){return e.map(t=>`
-  <li class="shopping-list-card" id="${t.id}">
+import"./assets/support-slider-ee0ae5e3.js";const l=document.querySelector(".shopping-list-cards");document.querySelector(".shopping-list-btn-delete");const i=document.querySelector(".shopping-list-storage");document.deleteCardItem=function(e){let s=a(n);s=s.filter(o=>o.id!==e),localStorage.setItem(n,JSON.stringify(s)),document.getElementById(""+e).remove(),s.length===0&&(l.style.display="none",i.style.display="block")};function p(t){return t.map(e=>`
+  <li class="shopping-list-card" id="${e.id}">
        <div class="shopping-list-card-img">
-          <img src="${t.book_image}" class="shopping-list-img" alt="${t.title}"/>
+          <img src="${e.book_image}" class="shopping-list-img" alt="${e.title}"/>
         </div>
         <div class="shopping-list-blok">
-          <h2 class="shopping-list-book-title">"${t.title}"</h2>
-          <p class="shopping-list-book-category">"${t.list_name}"</p>
-          <p class="shopping-list-book-description">"${t.description}"</p>
-          <p class="shopping-list-book-autor">"${t.author}"</p>
+          <h2 class="shopping-list-book-title">"${e.title}"</h2>
+          <p class="shopping-list-book-category">"${e.list_name}"</p>
+          <p class="shopping-list-book-description">"${e.description}"</p>
+          <p class="shopping-list-book-autor">"${e.author}"</p>
         </div>
         <ul class="shopping-list-shops">
-          <li class="shop-modal-item">
-              <a href="${marketAmazon}" target="_blank">
-                 <img class="amazon" width="62" height="19" srcset="${amazonJpg} 1x,${amazonJpg2x} 2x"
-                  src="${amazonJpg}"
-                  alt="Amazon"/>
-              </a>
-            </li>
-            <li class="shop-modal-item">
-              <a href="${marketAppleBooks}" target="_blank">
-                <img class="apple" width="33" height="32" srcset="${appleBookJpg} 1x,${appleBookJpg2x} 2x"
-                  src="${appleBookJpg}"
-                  alt="appleBookPng"/>
-              </a>
-            </li>
+          <li class="shopping-list-shop">
+            <a
+              href="${e.marketAmazon}"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+
+              <img src="./img/amazon.png" class="amazon" alt="${e.marketAmazon}" />
+
+            </a>
+          </li>
+          <li class="shopping-list-shop">
+            <a
+              href="${e.marketAppleBooks}"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+
+              <img src="./img/appleBook.png" class="appleBook" alt="${e.marketAppleBooks}" />
+
+            </a>
+          </li>
         </ul>
         <button
           type="button"
-          onClick = "deleteCardItem('${t.id}')"
+          onClick = "deleteCardItem('${e.id}')"
           class="shopping-list-btn-delete"
         >
           <svg class="shopping-list-btn-delete-icon" width="16" height="16">
             <use href="../img/sprit.svg#icon-trash"></use>
           </svg>
         </button>
-  </li>`).join("")}const a="storage-data";function o(e){const t=localStorage.getItem(e);return t===null?[]:JSON.parse(t)}function c(){const e=o(a);e&&e.length>0?(i.style.display="none",l.innerHTML=p(e)):(l.style.display="none",i.style.display="block")}c();
+  </li>`).join("")}const n="storage-data";function a(t){const e=localStorage.getItem(t);return e===null?[]:JSON.parse(e)}function r(){const t=a(n);t&&t.length>0?(i.style.display="none",l.innerHTML=p(t)):(l.style.display="none",i.style.display="block")}r();
 //# sourceMappingURL=commonHelpers2.js.map
